00c5245a80ac5c63fda97bbc0a1ac5b1070ad719
YuriiRodenko <jury.rodenko@gmail.com>
2023-07-09 19:43:38 +0300

    Revert "all_in"
    This reverts commit 1715dd7558aeb1bf007888e6876f799ff5e509ef.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,16 @@
-import React, { Component } from 'react';
-import ContactForm from './ContactForm';
-import ContactList from './ContactList';
-import Filter from './Filter';
-import { nanoid } from 'nanoid';
-import { Container, Title, SubTitle } from './App.styled';
-
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  addContact = ({ name, number }) => {
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-    this.state.contacts.find(
-      contact => contact.name.toLowerCase() === name.toLowerCase()
-    )
-      ? alert(`${name} is already in contacts.`)
-      : this.setState(({ contacts }) => ({ contacts: [...contacts, contact] }));
-  };
-
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  deleteContact = contactId => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  render() {
-    <div>
-      const { filter } = this.state;
-      const visibleContacts = this.getVisibleContacts();
-      return (
-        <Container>
-          <Title>Phonebook</Title>
-          <ContactForm onSubmit={this.addContact} />
-
-          <SubTitle>Contacts</SubTitle>
-          <Filter value={filter} onChange={this.changeFilter} />
-          <ContactList
-            contactsList={visibleContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        </Container>
-      );
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 36,
+        color: '#010101'
+      }}
+    >
+      React homework template
     </div>
-  }
-}
+  );
+};
